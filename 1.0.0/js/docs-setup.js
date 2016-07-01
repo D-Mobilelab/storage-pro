8e110fe23c85b3f83e61640fca4845ee6dda4c46
aureliome <aurelio.me@gmail.com>
2016-07-01 17:44:40 +0200

    Test 94%, package, bower

---
+++ 1.0.0/js/docs-setup.js
@@ -0,0 +1,25 @@
+NG_DOCS={
+  "sections": {
+    "version": "API Reference"
+  },
+  "pages": [
+    {
+      "section": "version",
+      "id": "Storage",
+      "shortName": "Storage",
+      "type": "object",
+      "moduleName": "Storage",
+      "shortDescription": "Storage is the library to save and get data in different ways.",
+      "keywords": "alert associated attributes browser console cookie cookies country data datas days debug default define defined delete described error exdays exhours exminutes expirations false function getmultiple global hours identify import info init initializing islocalstoragesupported islogged js jsobject key library load loads local localstorage log logger logs method methods methods_init minutes module multiple object options param returns save saved saves set setmultiple src start storage store stored support supported text true type undefined usage values variable variables verbose version warn ways"
+    }
+  ],
+  "apis": {
+    "version": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/version/Storage",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
