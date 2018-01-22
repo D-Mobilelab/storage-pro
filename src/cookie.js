ddaea012154f942c741bac82b4f9647a1f153a7a
Lorenzo <lorenzo.mussi@docomodigital.com>
2018-01-22 15:44:10 +0100

    cookie.js update for path

--- src/cookie.js
+++ src/cookie.js
@@ -72,6 +72,10 @@ module.exports = new function(){
                 d.setTime(d.getTime() + (options.exminutes * 60 * 1000));
                 newCookie += '; expires=' + d.toUTCString();
             }
+
+            if(options.path){
+                newCookie += '; path=' + options.path;
+            }
         }
             
         // set cookie
